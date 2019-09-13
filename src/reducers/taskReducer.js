f1b687da2ebc645c02ad3c04b726c083864415e9
Simon Gray <simongray2016@gmail.com>
2019-09-13 14:18:01 +0700

    save in localStorage

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,53 +1,85 @@
 import * as types from '../constants/actionTypes';
 import Task from '../model/Task';
+import Project from '../model/Project';
 import inbox from '../model/GetData';
 
 let initState = {
-    inbox,
+    taskList: new Project(),
     id: null,
 }
 
+let localList = JSON.parse(localStorage.getItem('taskList'));
+
+if (!localList) {
+    localStorage.setItem('taskList', JSON.stringify(inbox));
+    localList = JSON.parse(localStorage.getItem('taskList'));
+}
+
+initState.taskList.setList(localList.list);
+
+initState.taskList.setCompletedList(localList.completedList);
+
+initState.taskList.list = initState.taskList.list.map(task => {
+    let newDate = new Date(task.date);
+    return {...task, date: newDate}
+})
+
+initState.taskList.completedList = initState.taskList.completedList.map(task => {
+    let newDate = new Date(task.date);
+    return {...task, date: newDate}
+})
+
+
 const taskReducer = (state = initState, action) => {
-    let newState = Object.assign({}, state)
-    let { inbox, id } = newState;
     switch (action.type) {
         case types.ADD_TASK:
             let { task } = action;
             let newTask = new Task(task.name, task.date, task.priority);
-            inbox.list = inbox.addTask(newTask);
-            return { ...state, inbox };
+            state.taskList.list = state.taskList.addTask(newTask);
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
+            return { ...state };
         case types.COMPLETED_TASK:
-            inbox.list = inbox.completedTask(action.id);
-            inbox.completedList = inbox.addCompletedList();
+            state.taskList.list = state.taskList.completedTask(action.id);
+            state.taskList.addCompletedList();
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
             return { ...state };
         case types.EDIT_TASK:
-            let editList = inbox.editTask(action.id, action.task);
-            inbox.list = editList;
+            let editList = state.taskList.editTask(action.id, action.task);
+            state.taskList.list = editList;
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
             return { ...state }
         case types.DELETE_TASK:
-            inbox.list = inbox.deleteTask(action.id);
+            state.taskList.list = state.taskList.deleteTask(action.id);
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
             return { ...state }
         case types.CHANGE_PRIORITY:
-            inbox.list = inbox.changePriority(action.id, action.index);
+            state.taskList.list = state.taskList.changePriority(action.id, action.index);
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
             return { ...state }
         case types.FIND_TASK:
-            id = action.id
-            return { ...state, id }
+            state.id = action.id
+            return { ...state }
         case types.CLEAR_ID:
-            id = null;
-            return { ...state, id }
+            state.id = null;
+            return { ...state }
         case types.SET_DATE:
-            inbox.list = inbox.setDate(action.id, action.date);
-            return { ...state, inbox }
+            state.taskList.list = state.taskList.setDate(action.id, action.date);
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
+            return { ...state }
         case types.SORT_BY_DATE:
-            inbox.list = inbox.sortBy(action.value)
-            return { ...state, inbox };
+            state.taskList.list = state.taskList.sortBy(action.value);
+            return { ...state };
         case types.SORT_BY_PRIORITY:
-            inbox.list = inbox.sortBy(action.value);
-            return { ...state, inbox };
+            state.taskList.list = state.taskList.sortBy(action.value);
+            return { ...state };
         case types.SORT_BY_NAME:
-            inbox.list = inbox.sortBy(action.value);
-            return { ...state, inbox };
+            state.taskList.list = state.taskList.sortBy(action.value);
+            return { ...state };
+        case types.UNDO_TASK:
+            let undoTask = state.taskList.undoTask(action.id);
+            state.taskList.addTask(undoTask);
+            localStorage.setItem('taskList', JSON.stringify(state.taskList));
+            return { ...state };
         default:
             return state;
     }
