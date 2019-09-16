8ab47d370ddf15206bbfd980bb2e655818a3ca74
Simon Gray <simongray2016@gmail.com>
2019-09-16 13:16:53 +0700

    Updates

---
+++ precache-manifest.20c4ec85314d2a4a3b7b5be9222f387d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f72e2cd3717676c3b5403ded32b8f1",
+    "url": "/to-do-ist/index.html"
+  },
+  {
+    "revision": "7dc346a006dc77e4d297",
+    "url": "/to-do-ist/static/css/2.b4b6e04c.chunk.css"
+  },
+  {
+    "revision": "b155bf3482a68974d62d",
+    "url": "/to-do-ist/static/css/main.9e52bfef.chunk.css"
+  },
+  {
+    "revision": "7dc346a006dc77e4d297",
+    "url": "/to-do-ist/static/js/2.6971f4c2.chunk.js"
+  },
+  {
+    "revision": "b155bf3482a68974d62d",
+    "url": "/to-do-ist/static/js/main.46d6c3b5.chunk.js"
+  },
+  {
+    "revision": "dbbed755a305bfad2489",
+    "url": "/to-do-ist/static/js/runtime~main.42e0dd8c.js"
+  },
+  {
+    "revision": "5720cad5adaa37486c82913d45b5c347",
+    "url": "/to-do-ist/static/media/empty.5720cad5.svg"
+  }
+]);
